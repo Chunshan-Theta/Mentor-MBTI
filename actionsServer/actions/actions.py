faa8b7c45506412efd0b846dce83c04428170ac1
Wang <gavin19950511@gmail.com>
2023-10-19 08:59:57 +0000

    feat(db): add db framework

--- actionsServer/actions/actions.py
+++ actionsServer/actions/actions.py
@@ -11,10 +11,14 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from .models import callGPT_finetuneQuestion, callGPT_AnswerQuestion
+from .models import callGPT_AnswerQuestion
+from .db import getKN
+import json
 
 
+def send(d: CollectingDispatcher, obj: Any): d.utter_message(str(obj))
 def getStage(t: Tracker): return t.get_slot('stage')
+def getUserLatestMEG(t: Tracker): return t.latest_message
 
 class ActionFAQ(Action):
 
@@ -25,8 +29,14 @@ class ActionFAQ(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        text_latest_message="text_latest_message:"+str(tracker.latest_message)
-        dispatcher.utter_message(text=text_latest_message)
+        obj_TLM =getUserLatestMEG(tracker)
+
+        faqRanking: list[Dict[str,Any]] = obj_TLM["response_selector"]['faq']['ranking']
+        userText: str = obj_TLM["text"]
+        topOneKey: str = faqRanking[0]['intent_response_key'][4:]
+        topOneConf: float = faqRanking[0]['confidence']
+        examples: List[Dict[str, str]] = getKN(topOneKey)
+        dispatcher.utter_message(text=callGPT_AnswerQuestion(examples, userText))
 
         stage = getStage(tracker)
         # dispatcher.utter_message(text="get_slot(newQuestion): "+str(slotNewQuestion))
@@ -37,31 +47,4 @@ class ActionFAQ(Action):
         # dispatcher.utter_message(text=gptResponse)
         return [
             SlotSet("stage", "CustomAction")
-        ]
-
-class AnswerQuestion(Action):
-
-    def name(self) -> Text:
-        return "action_answer_question"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        # text_latest_message=f"text_latest_message: {tracker.latest_message}
-        slotNewQuestion = tracker.get_slot('newQuestion')
-        userContent = tracker.latest_message['text']
-
-        #
-        if slotNewQuestion is None or slotNewQuestion=="":
-            dispatcher.utter_message(text="我不明白的意思，請提出一個問題")
-            return []
-
-
-
-        #
-        gptResponse = callGPT_AnswerQuestion(slotNewQuestion)
-        dispatcher.utter_message(text=gptResponse)
-        return [
-            SlotSet("newQuestion", "")
         ]
--- actionsServer/actions/models.py
+++ actionsServer/actions/models.py
@@ -1,7 +1,11 @@
 import requests
 import json
-from typing import List, Any
+from typing import List, Any, Dict
 
+def genExample(example: Dict[str, str]): return  [
+    {"role": "user", "content": example["question"]},
+    {"role": "assistant", "content": example["answer"]}
+]
 def callGpt(
         userText: str,
         messages: List[Any],
@@ -32,30 +36,35 @@ def callGpt(
         return "ERROR:"+str(response.text)
 
 
-def callGPT_finetuneQuestion(userText: str) -> str:
-        return callGpt(userText, [
-                {
-                    "role": "system",
-                    "content": """
-                    我們是一個自然科的討論課程，你是這堂課的教師，你負責將學生的問題優化另一個問題，請理解學生的問題後提出更明確的問題。
-                    切記我們不提出自然以外的內容，所有提出的問題維持在自然領域之中。
-                    """
-                },
-                {"role": "user", "content": "蜘蛛 八隻腳"},
-                {"role": "assistant", "content": "請問蜘蛛是八隻腳嗎"},
-                {"role": "user", "content": userText}
-        ],0.3)
+# def callGPT_finetuneQuestion(userText: str) -> str:
+#         return callGpt(userText, [
+#                 {
+#                     "role": "system",
+#                     "content": """
+#                     我們是一個自然科的討論課程，你是這堂課的教師，你負責將學生的問題優化另一個問題，請理解學生的問題後提出更明確的問題。
+#                     切記我們不提出自然以外的內容，所有提出的問題維持在自然領域之中。
+#                     """
+#                 },
+#                 {"role": "user", "content": "蜘蛛 八隻腳"},
+#                 {"role": "assistant", "content": "請問蜘蛛是八隻腳嗎"},
+#                 {"role": "user", "content": userText}
+#         ],0.3)
 
-def callGPT_AnswerQuestion(userText: str) -> str:
-        return callGpt(userText, [
+def callGPT_AnswerQuestion(examples: List[Dict[str, str]],userText: str) -> str:
+
+        body: List[Dict[str,str]] = [
                 {
                     "role": "system",
                     "content": """
                     我們是一個自然科的討論課程，你是這堂課的教師，你負責回應學生的問題，請理解學生的問題後提出明確的。
                     切記我們不提出自然以外的內容，所有確保回應維持在自然領域之中，並在200個字內回答問題。
                     """
-                },
-                {"role": "user", "content": "請問蜘蛛是八隻腳嗎"},
-                {"role": "assistant", "content": "不是，大多數的蜘蛛是六隻腳"},
-                {"role": "user", "content": userText}
-        ],0.7)
+                }
+        ]
+
+        for e in examples:
+            body.extend(genExample(e))
+        
+        body.extend([{"role": "user", "content": userText}])
+        return callGpt(userText, body,0.7)
+        
